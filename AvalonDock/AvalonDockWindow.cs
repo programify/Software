ec0052d911968f0a20edc20db060286c3fce97a0
Clive Hudson <github@programify.com>
2019-05-20 17:05:15 +0100

    Initial Upload

---
+++ AvalonDock/AvalonDockWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace AvalonDock
+{
+    /// <summary>
+    /// nothing.
+    /// </summary>
+    public class AvalonDockWindow : Window
+    {
+        static AvalonDockWindow()
+        {
+            ShowInTaskbarProperty.OverrideMetadata(typeof(AvalonDockWindow), new FrameworkPropertyMetadata(false));
+        
+        }
+
+        internal AvalonDockWindow()
+        { }
+    }
+}
---
+++ AvalonDock/RequestDocumentCloseEventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace AvalonDock
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RequestDocumentCloseEventArgs : CancelEventArgs
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public RequestDocumentCloseEventArgs(DocumentContent doc)
+        {
+            DocumentToClose = doc;
+        }
+
+        /// <summary>
+        /// Document content that user wants to close
+        /// </summary>
+        public DocumentContent DocumentToClose { get; private set; }
+    }
+}
